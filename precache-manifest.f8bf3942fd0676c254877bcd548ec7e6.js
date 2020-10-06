e142506845642980d6979ae938315b7411a18365
Bakhodir Khakimov <bakhodir@MacBook-Pro-Bakhodir.local>
2020-10-06 23:06:45 +0200

    Updates

---
+++ precache-manifest.f8bf3942fd0676c254877bcd548ec7e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9400d5ecd4fb9e3d433af39d01e898a",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "6203004d9bcd64d3fc24",
+    "url": "/react-calculator/static/css/main.2322cc56.chunk.css"
+  },
+  {
+    "revision": "61d16786a0eae79292b7",
+    "url": "/react-calculator/static/js/2.b99e8b32.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator/static/js/2.b99e8b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6203004d9bcd64d3fc24",
+    "url": "/react-calculator/static/js/main.ab963ee5.chunk.js"
+  },
+  {
+    "revision": "b62fde247ff746a845a7",
+    "url": "/react-calculator/static/js/runtime-main.6e23a728.js"
+  }
+]);
